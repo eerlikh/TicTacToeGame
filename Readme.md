5f5449630c6892e1078063b1232ed6c36aa0d3ae
Eduard Erlikh <eerlikh7@gmail.com>
2015-08-06 12:17:10 -0400

    asdfasdf

---
+++ Readme.md
@@ -0,0 +1,23 @@
+working environment
+
+wander brain time
+-user stories
+-wireframes
+
+identify potential blocks
+-spike on them
+
+
+User story:
+
+After clicking the start button and selecting single or multiplayer,
+I want to be able to start a game of tic tac toe,
+so I can enjoy a game with a friend.
+
+After starting the game,
+I want the UI to be clear and easy to understand,
+so people can enjoy the game without trying to figure out how it works.
+
+As a player clicks a box on the grid,
+I want to be able to show an X or O based on the player,
+so I can keep track of who is doing what in the game.
--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-console.log("script loaded!")
+$(document).ready(function() {
+  console.log("script loaded!")
+
+  function playerOneLogic(){
+
+    // logic for 9 different boxes
+
+        $('.box, .One').click(function() {
+            $('.rowOneColOneX').toggle();
+        });
+
+        $('.box, .Two').click(function() {
+            $('.rowTwoColOneX').toggle();
+        });
+
+        $('.box, .Three').click(function() {
+            $('.rowTwoColOne').toggle();
+        });
+
+
+
+
+
+  }
+
+
+
+playerOneLogic();
+});
+
+
 
 //$( ".box" ).click(function() {
 //   console.log('yay');
@@ -30,7 +60,29 @@ console.log("script loaded!")
 //         control.attr("data-item-state", state == "showLess" ? "showMore" : "showLess");
 //     };
 // });
-  function changeState(){
-    var control = $(".box");
-    control.html("X" : "O");
-  }
+//   function changeState(){
+//     var control = $(".box");
+//     console.log(control);
+//     if(control==" "){
+//       control.html("X");
+//     } else if(control=="X"){
+//       control.html("O");
+//     } else if(control=="0"){
+//       control.html(" ");
+//   }
+// };
+
+//   jQuery.fn.extend({
+//   toggleText: function (a, b){
+//       var isClicked = false;
+//       var that = this;
+//       this.click(function (){
+//           if (isClicked) { that.text(a); isClicked = false; }
+//           else { that.text(b); isClicked = true; }
+//       });
+//       return this;
+//   }
+// });
+//
+// $('.box').toggleText("X", "O");
+//
