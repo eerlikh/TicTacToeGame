5c39872b2a95c91f41b4aaaae31adc01f70d3e6c
Eduard Erlikh <eerlikh7@gmail.com>
2015-08-09 21:17:08 -0400

    an update to the game logic that enhances multiplayer experience
    git push origin gh-pages
    reset
    saldfj;lasdkfjl;ajf
    
    as

--- Readme.md
+++ Readme.md
@@ -21,3 +21,7 @@ so people can enjoy the game without trying to figure out how it works.
 As a player clicks a box on the grid,
 I want to be able to show an X or O based on the player,
 so I can keep track of who is doing what in the game.
+
+If a player gets three X's or O's on the grid,
+I want to display a victory message and congratulate them and offer the two players to play again,
+so I can reward the players for their effort and give them another chance to battle to the death...
--- script.js
+++ script.js
@@ -1,88 +1,185 @@
 $(document).ready(function() {
-  console.log("script loaded!")
 
-  function playerOneLogic(){
+  console.log("script loaded!") // yay!!!
 
-    // logic for 9 different boxes
+//-------------------BELOW IS MULTIPLAYER LOGIC IN VS MODE
 
-        $('.box, .One').click(function() {
-            $('.rowOneColOneX').toggle();
-        });
+    var currentTurn = 0; //Variable that keeps track of turns.
+    var players = ['one', 'two'];
 
-        $('.box, .Two').click(function() {
-            $('.rowTwoColOneX').toggle();
-        });
-
-        $('.box, .Three').click(function() {
-            $('.rowTwoColOne').toggle();
-        });
-
-
-
-
-
-  }
-
-
-
-playerOneLogic();
+//-------------------Main Click Event that is the core of all logic
+    $('.box').mousedown(function(){
+      $(this).data('player', players[ currentTurn%2 ]); // Save data with the node
+      $(this).addClass('P-' + players[ currentTurn%2 ]);
+        if(currentTurn%2==0){
+              // console.log('inside player one  ' + currentTurn + " <-turn and this-> " + this); //tests conditional logic
+               playerOneLogic() //activates player one logic to place an O on the grid
+              // console.log('activate playerONE logic ' + currentTurn + " <-turn and this-> " + this); //tests player logic
+               currentTurn = currentTurn + 1;
+              // console.log(currentTurn%2); //makes sure the turn has been added
+               checkGameState()
+        } else {
+              // console.log('inside player two  ' + currentTurn + " <-turn and this-> " + this);
+               playerTwoLogic() //activates player two logic to place an O on the grid
+              // console.log('activate playerTWO logic ' + currentTurn + " <-turn and this-> " + this);
+               currentTurn++;
+              // console.log(currentTurn%2);
+               checkGameState()
+        }
+    });
 });
 
+function validateMove(){
 
 
-//$( ".box" ).click(function() {
-//   console.log('yay');
-//   $( "p" ).toggle();
-// });
+}
 
-//$("#foo").append("<div>hello world</div>")
-//
-// $(".box").on( "click", function() {
-//         alert('works');
-// });
-// //
-// $(document).ready(function() {
-//     $(".overview-continued").hide();
-//
-//     setState();
-//
-//     $(".show-overview").click(function() {
-//         $(".overview-continued").slideToggle("1000");
-//         setState();
-//         return false;
-//     });
-//
-//     function setState() {
-//         var control = $(".show-overview");
-//         var state = control.attr("data-item-state");
-//
-//         control.html(state == "showLess" ? "more..." : "less...");
-//         control.attr("data-item-state", state == "showLess" ? "showMore" : "showLess");
-//     };
-// });
-//   function changeState(){
-//     var control = $(".box");
-//     console.log(control);
-//     if(control==" "){
-//       control.html("X");
-//     } else if(control=="X"){
-//       control.html("O");
-//     } else if(control=="0"){
-//       control.html(" ");
-//   }
-// };
+function checkGameState(){
 
-//   jQuery.fn.extend({
-//   toggleText: function (a, b){
-//       var isClicked = false;
-//       var that = this;
-//       this.click(function (){
-//           if (isClicked) { that.text(a); isClicked = false; }
-//           else { that.text(b); isClicked = true; }
-//       });
-//       return this;
-//   }
-// });
-//
-// $('.box').toggleText("X", "O");
-//
+  var victoryConditions = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [1, 4, 8], [6, 4, 2] ];
+  var boards = $('.box').map(function(index, element){
+    return $(element).data('player') || 0;
+  });
+
+  var selection;
+  var i = i-1;
+  for (var i = 0; i < victoryConditions.length; i++) {
+    selection = victoryConditions[i].map(function(element){
+      return boards[element];
+    })
+
+  var victoryFilterPlayerOne =  selection.filter(function(victory){
+        for(var i=0; i<victory.length; i++){
+            if(victory[i]=="one", "one", "one"){
+            //    console.log(victory[i] " this is p1 victory array");
+                alert("Player one has won the game! Congratulations player one, you are officially superior to player two.  Play again??");
+            }
+            // else {
+            //   alert "Player two has won"
+            // }
+            //resetBoard()
+        }
+        return false;
+    });
+
+    console.log(victoryFilterPlayerOne);
+
+    var victoryFilterPlayerTwo =  selection.filter(function(victory){
+          for(var i=0; i<victory.length; i++){
+              if(victory[i]=="two", "two", "two"){
+              //    console.log(victory[i] " this is p2 victory array");
+                  alert("Player two has won the game! Congratulations player one, you are officially superior to player two. Play again?");
+                  resetBoard();
+                  return true;
+              }
+          }
+          return false;
+      });
+
+    console.log(victoryFilterPlayerTwo);
+
+    // ? are all the "selection" the same?
+    // if (selection===[i["one", "one", "one"]]){
+    //   alert("Player one has won the game! Congratulations player one, you are officially superior to player two. Maybe you want to play again to show this loser who is boss?");
+    // } else if (selection===[i["two", "two", "two"]]){
+    //   alert("Player one has won the game! Congratulations player one, you are officially superior to player two. Maybe you want to play again to show this loser who is boss?");
+    // }
+    // console.log(i);
+    // console.log([i["one", "one", "one"]]);
+    console.log(selection);
+  }
+
+}
+
+
+
+
+//------------------------------Boring functionality--------------------
+
+function playerOneLogic(){
+  // logic for 9 different boxes for PLAYER ONE
+
+      $('.One').mouseup(function() {
+        $('.rowOneColOne').html('X');
+      });
+
+      $('.Two').mouseup(function() {
+          $('.rowTwoColOne').html('X');
+      });
+
+      $('.Three').mouseup(function() {
+          $('.rowThreeColOne').html('X');
+      });
+
+      $('.Four').mouseup(function() {
+          $('.rowOneColTwo').html('X');
+      });
+
+      $('.Five').mouseup(function() {
+          $('.rowTwoColTwo').html('X');
+      });
+
+      $('.Six').mouseup(function() {
+          $('.rowThreeColTwo').html('X');
+      });
+
+      $('.Seven').mouseup(function() {
+          $('.rowOneColThree').html('X');
+      });
+
+      $('.Eight').mouseup(function() {
+          $('.rowTwoColThree').html('X');
+      });
+
+      $('.Nine').mouseup(function() {
+          $('.rowThreeColThree').html('X');;
+      });
+  return;
+  }
+
+  function playerTwoLogic(){
+    // logic for 9 different boxes for PLAYER TWO
+        $('.One').mouseup(function() {
+            $('.rowOneColOne').html('O');
+        });
+
+        $('.Two').mouseup(function() {
+            $('.rowTwoColOne').html('O');
+        });
+
+        $('.Three').mouseup(function() {
+            $('.rowThreeColOne').html('O');
+        });
+
+        $('.Four').mouseup(function() {
+            $('.rowOneColTwo').html('O');
+        });
+
+        $('.Five').mouseup(function() {
+            $('.rowTwoColTwo').html('O');
+        });
+
+        $('.Six').mouseup(function() {
+            $('.rowThreeColTwo').html('O');
+        });
+
+        $('.Seven').mouseup(function() {
+            $('.rowOneColThree').html('O');
+        });
+
+        $('.Eight').mouseup(function() {
+            $('.rowTwoColThree').html('O');
+        });
+
+        $('.Nine').mouseup(function() {
+            $('.rowThreeColThree').html('O');
+        });
+      return;
+      }
+
+      function resetBoard(){
+      // clears the board
+        $('.box').mouseup(function() {
+            $('.box').html(' ');
+        });
+      };
